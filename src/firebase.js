3d29794cc73a1e69af9f82b88c0d6f23b4abe8fb
hssw24 <hssw@gmx.de>
2024-07-23 07:53:53 +0200

    neu env back

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,20 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs, doc, updateDoc, setDoc } from 'firebase/firestore';
-import "dotenv/config"
+//import "dotenv/config"
 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBzyUUvOX_tWon7w3tcRX35tHRC3GkfY-A",
+    authDomain: "antischrei.firebaseapp.com",
+    databaseURL: "https://antischrei-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "antischrei",
+    storageBucket: "antischrei.appspot.com",
+    messagingSenderId: "1033095781501",
+    appId: "1:1033095781501:web:831488ad79931182ef6514"
+  };
+
+
+/*
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -11,7 +24,8 @@ const firebaseConfig = {
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID
   };
-  
+*/
+
 /*
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
